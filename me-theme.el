c4b37989336ecdc0320c1645daffb55bd525e147
dapft_mazzini <ktilaon@gmail.com>
2017-05-17 17:07:19 -0500

    deleted eshell dir & me-theme.el in master

--- me-theme.el
+++
@@ -1,8 +0,0 @@
-(deftheme me
-  "Created 2017-05-17.")
-
-(custom-theme-set-variables
- 'me
- '(custom-safe-themes (quote ("55e6f674b6633caf21358511167d6f15dcc8b6ad69f2eff766fafad9e9c73e07" "f9574c9ede3f64d57b3aa9b9cef621d54e2e503f4d75d8613cbcc4ca1c962c21" "38497ee2900c38900a6299792305475c614a344033c0e6329012e701c24aa924" default))))
-
-(provide-theme 'me)
