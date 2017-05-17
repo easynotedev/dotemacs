d129db321dd7070741158e605841e0bba05720b2
dapft_mazzini <ktilaon@gmail.com>
2017-05-17 17:04:41 -0500

    added custom-theme.el

---
+++ custom-theme.el
@@ -0,0 +1,17 @@
+(deftheme custom
+  "Created 2017-05-17.")
+
+(custom-theme-set-variables
+ 'custom
+ '(custom-safe-themes (quote ("f9574c9ede3f64d57b3aa9b9cef621d54e2e503f4d75d8613cbcc4ca1c962c21" "83fac9d1eb32b78d5f151ea1315ed66e0e688dd21ed340f5443dd737b187529c" default)))
+ '(package-selected-packages (quote (quote)))
+ '(horizontal-scroll-bar-mode t)
+ '(ansi-color-faces-vector [default default default italic underline success warning error])
+ '(indicate-empty-lines t)
+ '(package-archives (quote (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/") ("melpa" . "https://melpa.org/packages/")))))
+ '(speedbar-show-unknown-files t)
+ '(truncate-lines t)
+ '(global-visual-line-mode nil)
+ '(show-paren-mode t))
+
+(provide-theme 'custom)
