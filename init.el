bf516b7f3b2b066dcd42e41b825fd940151c6064
dapft_mazzini <ktilaon@gmail.com>
2017-06-15 21:53:41 -0500

    deleted unused helm intergration code with spaceemacs, aswell as helm autoresize

--- init.el
+++ init.el
@@ -268,23 +268,7 @@
 
 (helm-mode 1)
 
-(defun spacemacs//helm-hide-minibuffer-maybe ()
-  "Hide minibuffer in Helm session if we use the header line as input field."
-  (when (with-helm-buffer helm-echo-input-in-header-line)
-    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
-      (overlay-put ov 'window (selected-window))
-      (overlay-put ov 'face
-                   (let ((bg-color (face-background 'default nil)))
-                     `(:background ,bg-color :foreground ,bg-color)))
-      (setq-local cursor-type nil))))
-
-
-(add-hook 'helm-minibuffer-set-up-hook
-          'spacemacs//helm-hide-minibuffer-maybe)
-
-(setq helm-autoresize-max-height 0)
-(setq helm-autoresize-min-height 20)
-(helm-autoresize-mode 1)
+;;; DELETED helm integration codes with spacemacs
 
 ;;; Command helm-M-x
 (global-set-key (kbd "M-x") 'helm-M-x)
@@ -294,9 +278,6 @@
 
 ;;; helm projectile caching
 (setq projectile-enable-caching t)
-
-;;; Autoresize
-(helm-autoresize-mode t)
 
 ;;; Golden ratio.el
 ;;; https://github.com/roman/golden-ratio.el
