4a3ccda6ae24e1bbba6c61d8d5f513582906a713
dapft_mazzini <ktilaon@gmail.com>
2017-07-12 00:49:44 -0500

    added emmet-mode-hook for web-mode

--- init.el
+++ init.el
@@ -172,9 +172,10 @@
 ;; ///////////
 ;; Emmet-mode/
 (require 'emmet-mode)
+(add-hook 'web-mode-hook 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
-(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent 2 spaces.
+;; (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent 2 spaces.
 (setq emmet-move-cursor-between-quotes t) ;; default nil
 ;; If you want to use emmet with react-js's JSX,
 ;; you probably want emmet to expand 'className="..."' instead of 'class="..."':
