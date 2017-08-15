565e416b88ef4c6d070879384332490cdad45a03
Antonin Tilaon <ktilaon@gmail.com>
2017-08-15 10:55:33 -0500

    web-mode when .ejs

--- init.el
+++ init.el
@@ -178,6 +178,7 @@
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
 
 ;; Highlight current HTML element
 (setq web-mode-enable-current-element-highlight t)
